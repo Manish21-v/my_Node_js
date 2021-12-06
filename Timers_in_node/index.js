d75b8664640431d879936bc176bb9fb1aeeb6e1b
Manish21-v <vitkarmanish21@gmail.com>
2021-12-06 10:56:58 +0530

    added comments

--- Timers_in_node/index.js
+++ Timers_in_node/index.js
@@ -1,3 +1,17 @@
+/**Author : Manish
+ * Note : There are three types of Timers : timeout, interval and immediate
+ */
+
+
+/**
+ * SetTimeout(callback,'delay in millisceonds',[args]) : 
+ * This function returns a timer Object ID, you can pass this ID to clearTimeout(timeoutID)
+ * before the delayMillisecond expires to cancel the timeout function
+ */
+setTimeout(()=> {
+  console.log('Timeout function is executed');
+},'2000');
+
 var fs = require ("fs");
 
 fs.stat("nexttick.js",function() { // I/O operation
